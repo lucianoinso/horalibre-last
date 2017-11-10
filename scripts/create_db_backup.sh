602c27c5b20ae4887d8454aaaaf05ff38dea362a
lucianoinso <lucianoinso@gmail.com>
2017-11-10 05:23:09 +0000

    script stuff

--- scripts/create_db_backup.sh
+++ scripts/create_db_backup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-DEL_LAST_MONTH_DB_PATH_SCRIPT="${OPENSHIFT_REPO_DIR}/data/scripts/del_lastmonth_db_backups.sh"
-source $DEL_LAST_MONTH_DB_PATH_SCRIPT
+#DEL_LAST_MONTH_DB_PATH_SCRIPT="${OPENSHIFT_REPO_DIR}/data/scripts/del_lastmonth_db_backups.sh"
+#source $DEL_LAST_MONTH_DB_PATH_SCRIPT
 
-CURRENT_DATE=$(date +%d-%m-%y)
-DIRECTORY=${OPENSHIFT_DATA_DIR}db_backup
-FULL_PATH="${DIRECTORY}/${CURRENT_DATE}.sql"
-echo "Creating database backup..."
-echo $FULL_PATH
-if [ ! -d "$DIRECTORY" ]; then
-  mkdir $DIRECTORY
-fi
+#CURRENT_DATE=$(date +%d-%m-%y)
+#DIRECTORY=${OPENSHIFT_DATA_DIR}db_backup
+#FULL_PATH="${DIRECTORY}/${CURRENT_DATE}.sql"
+#echo "Creating database backup..."
+#echo $FULL_PATH
+#if [ ! -d "$DIRECTORY" ]; then
+#  mkdir $DIRECTORY
+#fi
 
 pg_dump -U $OPENSHIFT_POSTGRESQL_DB_USERNAME fundacion --clean > $FULL_PATH
+
+mysqldump -u fundacionhoralib -h fundacionhoralibre.mysql.pythonanywhere-services.com 'fundacionhoralib$db_11479' > db-backup.sql
---
+++ scripts/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mysqldump -u fundacionhoralib -h fundacionhoralibre.mysql.pythonanywhere-services.com 'fundacionhoralib$db_11479' > /home/fundacionhoralibre/db-backup.sql
