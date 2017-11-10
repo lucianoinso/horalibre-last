11bdf9686a75a8a19a61de54a06e35356d4a5d1e
lucianoinso <lucianoinso@gmail.com>
2017-11-10 20:38:16 -0300

    asdf

--- records/views.py
+++ records/views.py
@@ -51,8 +51,8 @@ def patient_list(request):
 #                                .distinct('patient').values_list('id', flat=True)
 
 # no anda bien
-#            cases = Case.objects.all().filter(Q(professional=prof) | Q(coordinator=prof))\
-#            .values_list('patient', flat=True).distinct()
+            cases = Case.objects.all().filter(Q(professional=prof) | Q(coordinator=prof))\
+            .values_list('patient', flat=True).distinct()
 
             case_list = Case.objects.filter(id__in=cases).order_by('patient__last_name')
 
